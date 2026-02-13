1a37c31a79b8bd2b93f7bc4579e2b8f7db02b7cc
Your Name <your-email@example.com>
2026-02-13 10:10:30 +0300

    Added route for InteractiveDemo page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import Profile from "./pages/Profile";
 import Habits from "./pages/Habits";
 import Journal from "./pages/Journal";
 import AIChat from "./pages/AIChat";
+import InteractiveDemo from "./pages/InteractiveDemo";
 import Medicine from "./pages/Medicine";
 import Nutrition from "./pages/Nutrition";
 import Movement from "./pages/Movement";
